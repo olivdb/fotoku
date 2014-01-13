23aea6aa36519a3ade137a24644bc274e0199caa
olivier <ovdbigge@gmail.com>
2014-01-13 18:43:53 +0400

    Quest to json

--- app/controllers/quests_controller.rb
+++ app/controllers/quests_controller.rb
@@ -3,7 +3,7 @@ class QuestsController < ApplicationController
   	@quests = Quest.all
   	#output_json = JSON.pretty_generate(JSON.parse(@quests.to_json))
   	#output_json = JSON.pretty_generate(@quests.map(&:attributes))
-  	json = @quests.to_json include: { owner: { only: [:id, :name] } }
+  	json = @quests.to_json include: { owner: { only: [:id, :name] } }, except: [:owner_id, :created_at, :updated_at]
   	json = JSON.pretty_generate(JSON.parse(json)) #only for debug!
   	render json: json
   end
