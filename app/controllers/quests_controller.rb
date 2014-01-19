2cb95a46a13d0e5b52adebaf25228c5b164467d4
Olivier van den Biggelaar <ovdbigge@gmail.com>
2014-01-20 00:05:41 +0400

    preliminary post quest

--- app/controllers/quests_controller.rb
+++ app/controllers/quests_controller.rb
@@ -1,10 +1,32 @@
 class QuestsController < ApplicationController
+  #before_action :signed_in_user
+  skip_before_filter :verify_authenticity_token 
+
   def index
   	@quests = Quest.all
   	#output_json = JSON.pretty_generate(JSON.parse(@quests.to_json))
   	#output_json = JSON.pretty_generate(@quests.map(&:attributes))
   	json = @quests.to_json include: { owner: { only: [:id, :name] } }, except: [:owner_id, :created_at, :updated_at]
   	json = JSON.pretty_generate(JSON.parse(json)) #only for debug!
+
   	render json: json
+  end 
+
+  def create
+  	@quest = User.find(1).quests.build(quest_params)
+    #@quest = current_user.quests.build(quest_params)
+    if @quest.save
+      render json: {}
+    else
+      render json: {}
+    end
   end
+
+  private
+
+    def quest_params
+      Rails.logger.debug("Received params: #{params.to_yaml}")
+      params.require(:quest).permit([:title, :photo_url])
+    end
+
 end
